7690c92ff243b85e9a457529e3319857a3aa9b0c
rafafnb1307 <rafafnb13@gmail.com>
2026-08-04 19:47:23 -0300

    Delete site2.0 directory

--- site2.0/reveal.js
+++
@@ -1,9 +0,0 @@
-/* Scroll-reveal for .reveal elements */
-(function () {
-  const els = document.querySelectorAll('.reveal');
-  if (!els.length) return;
-  const io = new IntersectionObserver((entries) => {
-    entries.forEach(e => { if (e.isIntersecting) { e.target.classList.add('visible'); io.unobserve(e.target); } });
-  }, { threshold: 0.12 });
-  els.forEach(el => io.observe(el));
-})();
